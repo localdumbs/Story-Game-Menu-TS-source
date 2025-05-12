4fed646d59a554a4b37cda7a34895adc6de013d8
localdumbs <113314528+localdumbs@users.noreply.github.com>
2025-05-12 13:30:54 -0700

    uhh add comments and some other logic i guess!

--- src/client/MenuCamera.client.ts
+++ src/client/MenuCamera.client.ts
@@ -1,29 +1,32 @@
 import { Players, RunService, Workspace, UserInputService, ReplicatedStorage } from "@rbxts/services";
-
+print("started Menu camera!-");
 const player = Players.LocalPlayer;
 const camera = Workspace.CurrentCamera!;
 const uhh = Workspace;
 const camPart = Workspace.WaitForChild("CameraPart") as BasePart;
 const ChosenLevel = script.FindFirstChildOfClass("ObjectValue");
 const Maps = ChosenLevel?.Value;
+let CurrentMenu = ChosenLevel?.Value;
 //const randoms = ran
 camera.CameraType = Enum.CameraType.Scriptable;
-
+// load the character, so shit don't break lmao! x3
 const character = player.Character || player.CharacterAdded.Wait()[0];
 import transition = require("shared/Menu-UI");
 //const fakemap = Maps?.Clone();
 if (Maps && Maps.IsA("Folder")) {
 	const fakemap = Maps.Clone();
 	fakemap.Parent = uhh;
+	CurrentMenu = fakemap;
 	print(fakemap);
 } else {
-	warn("ChosenLevel.Value is not set or not a Model.");
+	warn("ChosenLevel.Value is not set or is not valid.");
 }
 task.wait(1);
 transition.evadetransition();
 const maxTilt = 15;
 
 RunService.RenderStepped.Connect(() => {
+	// Runs everyframe to rotate the camera in the dirc of the mouse kinda shit but I dont care! x3
 	const viewportSize = camera.ViewportSize;
 	const mouseLocation = UserInputService.GetMouseLocation();
 
@@ -34,4 +37,16 @@ RunService.RenderStepped.Connect(() => {
 	const tiltY = math.rad(relativeX * -maxTilt);
 
 	camera.CFrame = camPart.CFrame.mul(CFrame.Angles(tiltX, tiltY, 0));
+	// this doesnt work and i have no idea why probally gonna move this to a module! x3
+	ChosenLevel?.GetPropertyChangedSignal("Value").Connect(() => {
+		if (Maps && Maps.IsA("Folder")) {
+			CurrentMenu?.Destroy();
+			const fakemap = Maps.Clone();
+			fakemap.Parent = uhh;
+			CurrentMenu = fakemap;
+			print(fakemap);
+		} else {
+			warn("ChosenLevel.Value is not set or is not valid.");
+		}
+	});
 });
