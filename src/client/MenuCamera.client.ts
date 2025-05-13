2841867d0849e3368ef96abc00e2f140ad611c54
localdumbs <113314528+localdumbs@users.noreply.github.com>
2025-05-13 11:25:58 -0400

    Update MenuCamera.client.ts

--- src/client/MenuCamera.client.ts
+++ src/client/MenuCamera.client.ts
@@ -1,5 +1,8 @@
+//-[Imports!]-\\
 import { Players, RunService, Workspace, UserInputService, ReplicatedStorage } from "@rbxts/services";
+
 print("started Menu camera!-");
+//-[Vars]-\\
 const player = Players.LocalPlayer;
 const camera = Workspace.CurrentCamera!;
 const uhh = Workspace;
@@ -8,6 +11,7 @@ const ChosenLevel = script.FindFirstChildOfClass("ObjectValue");
 const Maps = ChosenLevel?.Value;
 let CurrentMenu = ChosenLevel?.Value;
 //const randoms = ran
+//Character can camera stuff
 camera.CameraType = Enum.CameraType.Scriptable;
 // load the character, so shit don't break lmao! x3
 const character = player.Character || player.CharacterAdded.Wait()[0];
@@ -37,7 +41,7 @@ RunService.RenderStepped.Connect(() => {
 	const tiltY = math.rad(relativeX * -maxTilt);
 
 	camera.CFrame = camPart.CFrame.mul(CFrame.Angles(tiltX, tiltY, 0));
-	// this doesnt work and i have no idea why probally gonna move this to a module! x3
+	// this doesn't work and I have no idea why, probably gonna move this to a module! x3
 	ChosenLevel?.GetPropertyChangedSignal("Value").Connect(() => {
 		if (Maps && Maps.IsA("Folder")) {
 			CurrentMenu?.Destroy();
